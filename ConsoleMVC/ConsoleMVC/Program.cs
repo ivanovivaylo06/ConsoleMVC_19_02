c9dd7592cefd78bb680bf7d05a13816df9e2e35a
ivanovivaylo06 <ivakaplayz69@gmail.com>
2024-02-19 13:04:55 +0200

    Upload ConsoleMVC

---
+++ ConsoleMVC/ConsoleMVC/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleMVC.Constrollers;
+
+namespace ConsoleMVC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TipCalculatorController tipCalculatorController = new TipCalculatorController();
+        }
+    }
+}
